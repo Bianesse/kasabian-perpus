a2ee6e0238c361ae497b66ab3cab4757a2975dd4
Bianesse <ibrahimkasabian@gmail.com>
2025-02-21 07:29:43 +0700

    Fix: seeder fix

--- database/seeders/KasabianBookSeeder.php
+++ database/seeders/KasabianBookSeeder.php
@@ -14,8 +14,8 @@ class KasabianBookSeeder extends Seeder
     public function run(): void
     {
         Kasabian_book::insert([
-            ['kasabianJudul' => 'The Alchemist', 'kasabianPenulis' => 'Paulo Coelho', 'kasabianPenerbit' => 'Harper Collins', 'kasabianTahunTerbit' => '2015', 'kasabianDeskripsi' => 'An Andalusian shepherd boy named Santiago dreams of a treasure while in a ruined church. He consults a Gypsy fortune-teller about the meaning of the recurring dream. The woman interprets it as a prophecy, telling the boy that he will discover a treasure at the Egyptian pyramids.'],
-            ['kasabianJudul' => 'A Place Called Perfect', 'kasabianPenulis' => 'Hellena Duggan', 'kasabianPenerbit' => 'Bhuana Ilmu Populer', 'kasabianTahunTerbit' => '2022', 'kasabianDeskripsi' => "Violet never wanted to move to Perfect. Who wants to live in a town where everyone has to wear glasses to stop them going blind? And who wants to be tidy and perfectly behaved all the time? Violet quickly discovers there's something weird going on - her mam is acting strange and her dad has disappeared."],
+            ['kasabianJudul' => 'The Alchemist', 'kasabianPenulis' => 'Paulo Coelho', 'kasabianPenerbit' => 'Harper Collins', 'kasabianTahunTerbit' => '2015', 'kasabianDeskripsi' => 'An Andalusian shepherd boy named Santiago dreams of a treasure while in a ruined church. He consults a Gypsy fortune-teller about the meaning of the recurring dream. The woman interprets it as a prophecy, telling the boy that he will discover a treasure at the Egyptian pyramids.', 'stock' => 5],
+            ['kasabianJudul' => 'A Place Called Perfect', 'kasabianPenulis' => 'Hellena Duggan', 'kasabianPenerbit' => 'Bhuana Ilmu Populer', 'kasabianTahunTerbit' => '2022', 'kasabianDeskripsi' => "Violet never wanted to move to Perfect. Who wants to live in a town where everyone has to wear glasses to stop them going blind? And who wants to be tidy and perfectly behaved all the time? Violet quickly discovers there's something weird going on - her mam is acting strange and her dad has disappeared.", 'stock' => 10],
         ]);
     }
 }
